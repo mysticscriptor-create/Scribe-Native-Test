c77f8e01806a027910eb7ca96fd7a80421933fad
mysticscriptor-create <mysticscriptor@gmail.com>
2026-08-20 15:46:45 +0500

    Add files via upload

--- app/src/main/java/com/primaloptima/scribe/ui/editor/ScribeEditorLine.kt
+++ app/src/main/java/com/primaloptima/scribe/ui/editor/ScribeEditorLine.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
